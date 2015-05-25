f409485d1b26fcbf6043e285e454b4a227b8198b
Steve Miller <steventhomasmiller@gmail.com>
2015-05-25 00:49:16 -0700

    Getting npm start error. Thoughts, @thomaswilburn?

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("This space left blank.");
+var hapi = require("hapi");
+var server = new hapi.Server();
+server.connection({ port: 8000 });
+server.start();
+
+// server.views({
+//   path:
+// })
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: function(req, reply) {
+    reply("Hello from Hapi, Grunt and No Demon.");
+  }
+});
