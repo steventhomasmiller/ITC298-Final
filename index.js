596b4e62b8073252b03efa4a340a8dcf19ab6b27
Steve Miller <steventhomasmiller@gmail.com>
2015-05-25 12:38:10 -0700

    Basic shell set up.

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 var hapi = require("hapi");
 var server = new hapi.Server();
+var fs = require("fs");
+
 server.connection({ port: 8000 });
+
 server.start();
 
-// server.views({
-//   path:
-// })
+server.views({
+  path: "templates",
+  engines: {
+  	html: require("handlebars")
+  },
+  layoutPath: "layouts",
+  layout: "default",
+  partialsPath: "templates/partials",
+  isCached: false
+});
 
 server.route({
   method: "GET",
   path: "/",
   handler: function(req, reply) {
-    reply("Hello from Hapi, Grunt and No Demon.");
+    reply.view("index", {
+    	title: "Home"
+    });
+  }
+});
+
+server.route({
+	method: "GET",
+	path: "/example",
+	handler: function (req, reply){
+		fs.readFile("example.json",
+		"utf8", function(err, data) {
+		reply.view("example", {
+			title: "Example",
+			classes: JSON.parse(data)
+		});	
+	});
   }
 });
