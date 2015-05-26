426df43defdf862dcbbe8590c1fa3149da874680
Steve Miller <steventhomasmiller@gmail.com>
2015-05-25 17:51:25 -0700

    Some grunt files.

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+//Gruntfile.js
+
+module.exports = function(grunt){
+
+	grunt.registerTask("hello", function() {
+		console.log("Hellay from Grunt.");
+		grunt.file.write("build/test.txt",
+		"This file is written sync.")
+	});
+
+	grunt.registerTask("hi", ["hello"]);
+
+	grunt.loadNpmTasks("grunt-autoprefixer");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-concurrent");
+	grunt.loadNpmTasks("grunt-nodemon");
+
+	grunt.registerTask("default", ["autoprefixer", "concurrent"]);
+
+	grunt.initConfig({
+		concurrent: {
+			dev: {
+				tasks: ["watch", "nodemon"],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		},
+		nodemon: {
+			dev: {
+				script: "index.js"
+			}
+		},
+		watch: {
+			prefix: {
+				files: "src/css/**/*.css",
+				tasks: ["autoprefixer"]
+			},
+			options: {
+				livereload: true
+			},
+			template: {
+				files: "**/*.html",
+				tasks: ["hello"]
+			}
+		},
+		autoprefixer: {
+			dev: {
+				expand: true,
+				flatten: true,
+				src: "src/css/**/*.css",
+				dest: "build/css/"
+			}
+		}
+	});
+};
