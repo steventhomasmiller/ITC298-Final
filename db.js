722f49bb1d7bc295c8dc66c1818fdfe476cfec6a
Steve Miller <steventhomasmiller@gmail.com>
2015-05-28 18:59:17 -0700

    Some more database base stuff.

--- db.js
+++ db.js
@@ -10,4 +10,8 @@ var blogpost = new sqlite.Database("blogpost.db", function() {
 
 });
 
+db.get("SELECT * FROM blogpost", function(err, result){
+	console.log("result");
+});
+
 module.exports = blogpost;
